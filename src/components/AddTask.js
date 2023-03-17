d6e769153793e3e4928530334eecdd72df61edbc
Vilmor Somera <vsomera77@outlook.com>
2023-03-16 21:50:06 -0700

    created form input react states

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,36 @@
+import { useState } from 'react'
+
 const AddTask = () => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
     return (
     <form className="add-form">
         <div className="form-control">
             <label>Task</label>
-            <input type="text" placeholder="Add Name of task" />
+            <input type="text" 
+            placeholder="Add Name of task" 
+            value={text} 
+            onChange={(e) => setText(e.target.value)} /> {/* Targets the value inside the texbox, returns a string */}
         </div>
+
         <div className="form-control">
             <label>Day & Time</label>
-            <input type="text" placeholder="Add Day & Time" />
+            <input type="text" 
+            placeholder="Add Day & Time" 
+            value={day} 
+            onChange={(e) => setDay(e.target.value )} /> {/* Targets the value inside the texbox, returns a string */}
         </div>
-        <div className="form-control">
+
+        <div className="form-control form-control-check">
             <label>Set Reminder</label>
-            <input type="checkbox" placeholder="Add Name of task" />
+            <input type="checkbox" 
+            placeholder="Add Name of task" 
+            value={reminder} 
+            onChange={(e) => setReminder(e.currentTarget.checked)} /> {/* Targets the check box returning a boolean */}
         </div>
-        <input type="submit" value='Save Task'></input>
+        <input type="submit" value='Save Task'className="btn btn-block" />
     </form>
     )
 }
