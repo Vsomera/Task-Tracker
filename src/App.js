1c8a373666beeadc5080e7d57b98addf1e76ed07
Vilmor Somera <vsomera77@outlook.com>
2023-04-17 22:42:23 -0700

    implemented mock back-end server using npm json-server + delete requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react' // React Hook
+import { useState, useEffect } from 'react' // React Hook
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -9,26 +9,29 @@ function App() {
 
   const [showAddTask, setShowAddTask] = useState(false) // state for showing the add task form
 
-  const [tasks, setTasks] = useState([ // list of tasks, setTasks is used to modify contents of the list
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-    {
-      id: 2,
-      text: "Food Shopping",
-      day: "Feb 6th at 1:30pm",
-      reminder: false,
-    },
-    {
-      id: 3,
-      text: "Meeting at School",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-  ])
+  const [tasks, setTasks] = useState([]) // list of tasks, setTasks is used to modify contents of the list
+
+  // React hook that fetches tasks from json server (db.json)
+  useEffect(() => {
+    
+    const getTasks = async () => {
+      const taskFromServer = await fetchTasks()
+      setTasks(taskFromServer)
+    }
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks") // fetches task data from server
+    // if there is an error while fetching the response throw an error
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await res.json() // translates response data into json
+    return data
+  }
 
   const toggleAddForm = () => {
     // When called, toggles the Add form
@@ -40,9 +43,14 @@ function App() {
     const newTask = { id, ...task } // creates a new object containing the task text, day, and reminder(dict)
     setTasks([...tasks, newTask]) // updates the current tasks state by adding the new task
     console.log("Added a new task: ", ...newTask)
+
+    // TODO : Send Post requests to add tasks
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    // Sends a delete request to the server to delete a task that matches id param
+    await fetch(`http://localhost:5000/tasks/${id}`, { method : "DELETE" })
+
     // Deleting Tasks by filtering through tasks
     setTasks(tasks.filter((task) => task.id !== id)) // removes task if id param matches
     console.log("Deleted Task with ID: ", id) // logs the deleted task in the console 
@@ -51,28 +59,30 @@ function App() {
 
   const toggleReminder = (id) => {
     // Toggles the reminder
-    setTasks(tasks.map((task) => 
-      task.id === id ? {...task, reminder: 
-      !task.reminder } : task   // toggles the reminder in task from true to false and from false to true
+    setTasks(tasks.map((task) =>
+      task.id === id ? {
+        ...task, reminder:
+          !task.reminder
+      } : task   // toggles the reminder in task from true to false and from false to true
     ))
     console.log("Toggled task", id)
   }
 
   return (
     <div className="container">
-      <Header 
-      toggleForm={toggleAddForm}
-      btnColor = {showAddTask ? "red" : "green"}
-      btnText = {showAddTask ? "Close" : "Add Task"}
+      <Header
+        toggleForm={toggleAddForm}
+        btnColor={showAddTask ? "red" : "green"}
+        btnText={showAddTask ? "Close" : "Add Task"}
       />
 
 
       {showAddTask && <AddTask onAdd={addTask} />} {/* If showAddTask is True, show add task form */}
-        {tasks.length > 0 ? 
-          <Tasks tasks={tasks}           // Sends tasks list to Tasks.js component
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks}           // Sends tasks list to Tasks.js component
           onDelete={deleteTask}
-          onToggle = {toggleReminder}/> 
-            : "No Tasks to Show"} {/* Deletes a task when called with a task id from Task.js component */}
+          onToggle={toggleReminder} />
+        : "No Tasks to Show"} {/* Deletes a task when called with a task id from Task.js component */}
     </div>
   )
 }
