86995141177e17f95ebdc46e145962fe8a3ff1a8
Vilmor Somera <vsomera77@outlook.com>
2023-04-18 18:53:54 -0700

    implemented post request for adding task to server

--- src/App.js
+++ src/App.js
@@ -13,13 +13,14 @@ function App() {
 
   // React hook that fetches tasks from json server (db.json)
   useEffect(() => {
-    
+
     const getTasks = async () => {
+      // sends a get request to server 
       const taskFromServer = await fetchTasks()
       setTasks(taskFromServer)
     }
     getTasks()
-  }, [])
+  }, []) // executes on browser load
 
   // Fetch Tasks
   const fetchTasks = async () => {
@@ -38,18 +39,28 @@ function App() {
     showAddTask ? setShowAddTask(false) : setShowAddTask(true)
   }
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1 // generates an id number for the task
-    const newTask = { id, ...task } // creates a new object containing the task text, day, and reminder(dict)
-    setTasks([...tasks, newTask]) // updates the current tasks state by adding the new task
-    console.log("Added a new task: ", ...newTask)
+  const addTask = async (task) => {
 
-    // TODO : Send Post requests to add tasks
+    const id =  Math.floor(Math.random() * 1000) + 1 // generates unique id number for the task
+    const newTask =  { id, ...task } // creates a new object containing the task text, day, and reminder(dict)
+
+    // Send Post requests to add tasks
+    await fetch("http://localhost:5000/tasks", {
+      method: "POST", // sends a post request to server
+      headers: {
+        Accept: 'application.json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTask), // sends the task to the server
+      Cache: 'default'
+    })
+
+    setTasks([...tasks, newTask]) // updates the current tasks state by adding the new task
   }
 
   const deleteTask = async (id) => {
     // Sends a delete request to the server to delete a task that matches id param
-    await fetch(`http://localhost:5000/tasks/${id}`, { method : "DELETE" })
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" })
 
     // Deleting Tasks by filtering through tasks
     setTasks(tasks.filter((task) => task.id !== id)) // removes task if id param matches
